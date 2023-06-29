5c38b5a41e0f8fb2f4f844b0326ef7fe5f8538ba
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2023-06-29 11:35:24 +0300

    selec click

--- src/component/filter/modules/select-period/Select-period.js
+++ src/component/filter/modules/select-period/Select-period.js
@@ -1,8 +1,11 @@
 import './Select-period.scss'
+import React from "react";
 
 function SelectPeriod({data}){
     const now = new Date();
     let day;
+    let refDateStart = React.createRef();
+    let refDateEnd = React.createRef();
     if((Number(now.getMonth()) +1) < 10){
         day = '0' + (Number(now.getMonth()) +1);
     } else {
@@ -21,17 +24,20 @@ function SelectPeriod({data}){
         maxDate = '';
         minDate = '';
     }
+    function inputSelect(ref){
+        ref.current.showPicker();
+    };
     return(
         <div className='gs-select-period'>
             <label className='gs-select-period__label'>
                 <span className="gs-select-period__label__pre">с</span>
-                <input type="date" name={`${data.for}-date-start`} min={minDate} max={maxDate}/>
-                <div className='gs-icon icon__calendar'></div>
+                <input ref={refDateStart} type="date" name={`${data.for}-date-start`} min={minDate} max={maxDate}/>
+                <div className='gs-icon icon__calendar' onClick={() => inputSelect(refDateStart)}></div>
             </label>
             <label className='gs-select-period__label'>
                 <span className="gs-select-period__label__pre">до</span>
-                <input type="date" name={`${data.for}-date-end`} min={minDate} max={maxDate}/>
-                <div className='gs-icon icon__calendar'></div>
+                <input ref={refDateEnd} type="date" name={`${data.for}-date-end`} min={minDate} max={maxDate}/>
+                <div className='gs-icon icon__calendar' onClick={() => inputSelect(refDateEnd)}></div>
             </label>
         </div>
     );
